9bebe5122f97df331612674200d8381be1630086
johnhodge <jrhodg17@g.holycross.edu>
2021-03-14 18:26:32 -0400

    "Add preface and fix 'By failing to' dilemna"

--- main.js
+++ main.js
@@ -6,6 +6,12 @@
 // create a function that grabs a random element from a given array. create three vars the is the result of running the function for
 // each of the arrays. console.log a string that is the concatenation of each var - specifically in order of the arrays.
 
+const preface = [
+    'I swore I once heard Ben say: ',
+    'Many years ago, Ben told me... ',
+    'There\'s a Ben Franklin quote that goes something like: '
+]
+
 const first = [
     'Well done is better than to ',
     'It takes many good deeds to ',
@@ -23,23 +29,35 @@ const second = [
 ]
 
 const third = [
-    'alone.', 
-    'together.',
-    'indisposed.',
-    'without pants on.',
-    'in the crick well past dawn.'
+    'alone', 
+    'together',
+    'indisposed',
+    'without pants on',
+    'in the crick well past dawn'
+]
+
+const justInCase = [
+    'fly.',
+    'scream.',
+    'faint.',
+    'lose your FUCKING mind!'
 ]
 
 const grabRandElement = (arr) => {
     return arr[Math.floor(Math.random() * arr.length)];
 }
 
-const createString = (arr1, arr2, arr3) => {
+const createString = (pref, arr1, arr2, arr3, suffix) => {
+    const prefaceQuote = grabRandElement(pref)
     const firstPart = grabRandElement(arr1);
     const secondPart = grabRandElement(arr2);
     const thirdPart = grabRandElement(arr3);
+    const mightNeedThisToo = grabRandElement(suffix);
 
-    return firstPart + secondPart + thirdPart;
+    if (firstPart === 'By failing to ') {
+        return `${prefaceQuote}${firstPart}${secondPart}${thirdPart} you will eventually ${mightNeedThisToo}`
+    }
+    return prefaceQuote + firstPart + secondPart + thirdPart + '.';
 }
 
-console.log(createString(first, second, third));
+console.log(createString(preface, first, second, third, justInCase));
